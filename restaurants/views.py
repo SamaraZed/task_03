5c499d7ec4acce99eb89741235c8e681c988cab5
Ayham Samara <Ayham.Samara@live.com>
2020-02-12 14:09:35 +0200

    Task 3. Tests successful

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,9 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    "my_list" : [{"restaurant_name":"Burgerizz", "food_type":"Burgers and Wings"}
+    ,{"restaurant_name":"Hamada", "food_type":"Falafel"}
+    ,{"restaurant_name":"Shawermati", "food_type":"Shawarma"}]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +16,6 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    "my_object":{"restaurant_name":"Falafelrest","food_type":"Mansaf"}
     }
     return render(request, 'detail.html', context)
